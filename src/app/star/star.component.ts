c4378da208810ecba02feec86308ccee4737fc8d
Daniel Christofolli <danielchristofolli@gmail.com>
2021-02-18 08:33:03 -0300

    Criação do componente star

--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnChanges, SimpleChanges } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 
 @Component({
-    selector: 'app-star'
+    selector: 'app-star',
+    templateUrl: './star.component.html'
 })
 export class StarComponent implements OnChanges {
+
+    @Input()
+    rating: number = 0;
+
+    starWidth?: number;
+
     ngOnChanges(): void {
-        
+        this.starWidth = this.rating * 94 / 5;
     }
 
 }
